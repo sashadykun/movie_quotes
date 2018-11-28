e3f768db8a71fd6220aaf4f99cb73f740cd1290d
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-28 13:36:38 -0800

    project setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+
+
+const store = createStore(rootReducer, {}, applyMiddleware())
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
---
+++ src/reducers/quotes_reducer.js
@@ -0,0 +1,12 @@
+import types from '../actions/types';
+
+const DAFAULT_STATE = {
+
+};
+
+export default (state = DAFAULT_STATE, action) => {
+    switch(action.type){
+        default:
+        return state;
+    }
+}
