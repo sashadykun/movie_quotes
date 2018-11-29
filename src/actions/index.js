5ef8a46c48e66fc3ef7d0627587999681939d077
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-28 17:04:54 -0800

    complete fake sign in and sign out

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,23 @@
 import types from './types';
 import axios from 'axios';
+
+
+export function userSignIn(){
+    console.log('user sign in called:');
+        return {
+            type: types.SIGN_IN
+        }
+}
+
+export function userSignOut(){
+    return {
+        type: types.SIGN_OUT
+    }
+}
+
+export function userSignUp(){
+    return {
+        type: types.SIGN_UP
+    }
+}
+
