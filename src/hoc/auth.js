a658190c41fd5886904b582f85672c57ab30f2fc
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-28 18:10:26 -0800

    create auth high order component

---
+++ src/hoc/auth.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+export default function(WrappedComponent){
+
+    
+
+    class Auth extends Component {
+        componentDidMount(){
+            console.log('auth Hoc', this.props)
+            this.checkAuth();
+        }
+        componentDidUpdate(){
+            this.checkAuth();
+        }
+
+checkAuth(){
+    if(!this.props.auth){
+        this.props.history.push('/sign-in');
+    }
+}
+
+        render(){
+            
+            return <WrappedComponent {...this.props}/>
+        }
+    }
+    function mapStateToProps(state){
+        return {
+            auth: state.user.auth
+        }
+    }
+    return connect(mapStateToProps)(Auth);
+}
